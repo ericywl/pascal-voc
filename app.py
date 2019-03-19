e080657db6b236fcec167b0be5f74530f055e789
ericywl <ericywl@live.com>
2019-03-19 18:14:26 +0800

    Update predict page for web GUI

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, jsonify
+
+from pascal import PascalClassifier
+
 app = Flask(__name__)
+pc = PascalClassifier(weights_path="weights/five_crop_weights.pth")
+
 
 @app.route("/")
 def main():
     return render_template("index.html")
-    
-@app.route("/predict")
+
+
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    return render_template("predict.html")
+    if request.method == "GET":
+        return render_template("predict.html")
+    if request.method == "POST":
+        # TODO: add image to local and do prediction
+        return jsonify({"test": "hello"})
+    # Invalid HTTP request method
+    return ("", 400, "Invalid request")
+
 
 @app.route("/ranks")
 def ranks():
---
+++ static/js/predict.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+    document.getElementById("files").onchange = function() {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // Get loaded data and render thumbnail.
+            document.getElementById("image").src = e.target.result;
+        };
+        // Read the image file as a data URL.
+        reader.readAsDataURL(this.files[0]);
+        document.getElementById("files-label").innerText = this.files[0].name;
+        document.getElementById("predict-btn").disabled = false;
+    };
+
+    document.getElementById("predict-btn").onclick = function() {
+        file = document.getElementById("files").files[0];
+        // Check file type is image
+        if (file.type.indexOf("image") < 0) {
+            console.log("File must be an image!");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const imgBlob = e.target.result;
+            const json = { name: file.name, data: imgBlob };
+            makePostRequest("/predict")
+                .then(function(response) {
+                    console.log(response);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        };
+        reader.readAsDataURL(file);
+    };
+};
+
+const makePostRequest = function(url, json) {
+    const xhr = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+        xhr.onreadystatechange = function() {
+            // Request not ready
+            if (xhr.readyState !== 4) return;
+            // Process response
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr);
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+
+        xhr.open("POST", url, true);
+        xhr.send(json);
+    });
+};
